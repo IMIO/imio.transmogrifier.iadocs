30d1ea889f9d01174673a2b9d0a63fdb7434fd8a
Stephan Geulette <s.geulette@imio.be>
2023-02-09 14:27:24 +0100

    Added simulate mode and optional where clause

--- src/imio/transmogrifier/iadocs/scripts/mssqlcmd_to_fwf.py
+++ src/imio/transmogrifier/iadocs/scripts/mssqlcmd_to_fwf.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 """Script to run sqlcmd commands to export tables"""
+from collections import OrderedDict
 from datetime import datetime
 from imio.pyutils.system import runCommand
 
@@ -13,16 +14,24 @@ logging.basicConfig()
 logger = logging.getLogger('sqlcmd')
 logger.setLevel(logging.INFO)
 sqlcmd_ext = '.fwf'
-tables = ['eAdresses', 'eCourriers', 'eClassement', 'eClassementDossiers', 'eContacts', 'eContactsAdresses',
-          'eContactsTitres', 'eContactsType', 'eCourriersDestinataires', 'eCourriersFichiers', 'eCourriersDossiers',
+tableX = ['eAdresses', 'eClassement', 'eClassementDossiers', 'eContacts', 'eContactsAdresses', 'eContactsTitres',
+          'eContactsType', 'eCourriers', 'eCourriersDestinataires', 'eCourriersFichiers', 'eCourriersDossiers',
           'eCourriersLiens', 'eCourriersServices', 'eGroupes', 'eGroupesContacts', 'eGroupesMembres', 'eNatures',
           'eRues', 'eServices', 'eTypeAction', 'eUsers']
+tables = OrderedDict([
+    ('eAdresses', {'c': ''}), ('eClassement', {'c': ''}), ('eClassementDossiers', {'c': ''}), ('eContacts', {'c': ''}),
+    ('eContactsAdresses', {'c': ''}), ('eContactsTitres', {'c': ''}), ('eContactsType', {'c': ''}),
+    ('eCourriers', {'c': "TypeEntrantSortant in ('E', 'S') and isnull(Supprime, '0') != '1'"}),
+    ('eCourriersDestinataires', {'c': ''}), ('eCourriersFichiers', {'c': ''}), ('eCourriersDossiers', {'c': ''}),
+    ('eCourriersLiens', {'c': ''}), ('eCourriersServices', {'c': ''}), ('eGroupes', {'c': ''}),
+    ('eGroupesContacts', {'c': ''}), ('eGroupesMembres', {'c': ''}), ('eNatures', {'c': ''}), ('eRues', {'c': ''}),
+    ('eServices', {'c': ''}), ('eTypeAction', {'c': ''}), ('eUsers', {'c': ''})])
 fwf_cmd = 'docker exec -u root -it {dock} /opt/mssql-tools/bin/sqlcmd -S localhost -d {db} -U SA -P "{pwd}" ' \
-          '-Q "select * from {table}" -o "/srv/sqlcmd.fwf" -s"{sep}"'
+          '-Q "select * from {table}{where}" -o "/srv/sqlcmd.fwf" -s"{sep}"'
 cp_cmd = 'docker cp {dock}:/srv/sqlcmd.fwf "{of}"'
 
 
-def main(docker, db_name, pwd, delim, input_filter, output_dir, only_new):
+def main(docker, db_name, pwd, delim, input_filter, output_dir, only_new, simulate):
     start = datetime.now()
     logger.info("Start: {}".format(start.strftime('%Y%m%d-%H%M')))
     for table in tables:
@@ -31,8 +40,16 @@ def main(docker, db_name, pwd, delim, input_filter, output_dir, only_new):
         out_file = os.path.join(output_dir, '{}{}'.format(table, sqlcmd_ext))
         if only_new and os.path.exists(out_file):
             continue
-        logger.info("ON sql table or view '{}'".format(table))
-        cmd = fwf_cmd.format(**{'dock': docker, 'db': db_name, 'pwd': pwd, 'table': table, 'sep': delim})
+        where = ''
+        if tables[table]['c']:
+            where = ' where {}'.format(tables[table]['c'])
+        cmd = fwf_cmd.format(**{'dock': docker, 'db': db_name, 'pwd': pwd, 'table': table, 'sep': delim,
+                                'where': where})
+        if simulate:
+            logger.info(cmd)
+            continue
+        else:
+            logger.info("ON sql table or view '{}'".format(table))
         logger.debug("cmdsql='{}'".format(cmd))
         (out, err, code) = runCommand(cmd)
         if code or err:
@@ -57,7 +74,8 @@ if __name__ == '__main__':
     parser.add_argument('-od', '--output_dir', dest='output_dir', help='Output directory. Default "."', default='.')
     parser.add_argument('-on', '--only_new', dest='only_new', action='store_true',
                         help='Export only not existing fwf files.')
+    parser.add_argument('-s', '--simulate', dest='simulate', action='store_true', help='Simulate operation')
     ns = parser.parse_args()
     if not ns.database:
         ns.database = ns.docker
-    main(ns.docker, ns.database, ns.password, ns.output_sep, ns.input_filter, ns.output_dir, ns.only_new)
+    main(ns.docker, ns.database, ns.password, ns.output_sep, ns.input_filter, ns.output_dir, ns.only_new, ns.simulate)
