4650e088d7688d398ca3925dfe02835297962b7b
Stephan Geulette <s.geulette@imio.be>
2023-02-09 12:30:02 +0100

    Added count section

--- src/imio/transmogrifier/iadocs/blueprints/various.py
+++ src/imio/transmogrifier/iadocs/blueprints/various.py
@@ -7,6 +7,7 @@ from imio.transmogrifier.iadocs import ANNOTATION_KEY
 from collective.transmogrifier.interfaces import ISection
 from collective.transmogrifier.interfaces import ISectionBlueprint
 from collective.transmogrifier.utils import Condition
+from Products.CMFPlone.utils import safe_unicode
 from zope.annotation.interfaces import IAnnotations
 from zope.interface import classProvides
 from zope.interface import implements
@@ -15,7 +16,7 @@ import ipdb
 # import sys
 
 
-class BreakpointSection(object):
+class Breakpoint(object):
     """Stops with ipdb if condition is matched.
 
     Parameters:
@@ -39,6 +40,38 @@ class BreakpointSection(object):
             yield item
 
 
+class Count(object):
+    """Count items."""
+    classProvides(ISectionBlueprint)
+    implements(ISection)
+
+    def __init__(self, transmogrifier, name, options, previous):
+        self.previous = previous
+        self.name = name
+        self.transmogrifier = transmogrifier
+        self.storage = IAnnotations(transmogrifier).get(ANNOTATION_KEY)
+        if 'count' not in self.storage:
+            self.storage['count'] = {}
+        self.storage['count'][name] = {}
+        self.group_key = safe_unicode(options.get('group_key', ''))
+
+    def __iter__(self):
+        counter = self.storage['count'][self.name]
+        for item in self.previous:
+            if self.group_key:
+                if self.group_key in item:
+                    counter.setdefault(item[self.group_key], {'c': 0})['c'] += 1
+                else:
+                    counter.setdefault('{}_missing'.format(self.group_key), {'c': 0})['c'] += 1
+            else:
+                counter.setdefault('', {'c': 0})['c'] += 1
+            yield item
+        for group in counter:
+            o_logger.info("{} '{}' = {}".format(self.name, group, counter[group]['c']))
+        # for group in self.storage['count'][self.name]:
+        #     o_logger.info("{} '{}' = {}".format(self.name, group, self.storage['count'][self.name][group]['c']))
+
+
 class ShortLog(object):
     """Logs shortly item."""
     classProvides(ISectionBlueprint)
@@ -57,7 +90,7 @@ class ShortLog(object):
             yield item
 
 
-class StopSection(object):
+class Stop(object):
     """Stops if condition is matched.
 
     Parameters:
