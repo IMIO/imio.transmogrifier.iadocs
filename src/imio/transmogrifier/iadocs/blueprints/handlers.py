36171bfdaddcb633a0c18747cff1628563c32c28
Stephan Geulette <s.geulette@imio.be>
2023-02-20 13:15:43 +0100

    Started personnel-folder creation

--- src/imio/transmogrifier/iadocs/blueprints/handlers.py
+++ src/imio/transmogrifier/iadocs/blueprints/handlers.py
@@ -2,6 +2,8 @@
 from collective.transmogrifier.interfaces import ISection
 from collective.transmogrifier.interfaces import ISectionBlueprint
 from collective.transmogrifier.utils import Condition
+from imio.helpers.content import uuidToObject
+from imio.helpers.transmogrifier import correct_path
 from imio.helpers.transmogrifier import filter_keys
 from imio.transmogrifier.iadocs import ANNOTATION_KEY
 from imio.transmogrifier.iadocs.utils import get_mailtypes
@@ -13,9 +15,14 @@ from imio.transmogrifier.iadocs.utils import log_error
 from imio.transmogrifier.iadocs.utils import MAILTYPES
 from plone import api
 from Products.CMFPlone.utils import safe_unicode
+from z3c.relationfield import RelationValue
 from zope.annotation import IAnnotations
+from zope.component import getUtility
 from zope.interface import classProvides
 from zope.interface import implements
+from zope.intid import IIntIds
+
+import os
 
 
 class AServiceUpdate(object):
@@ -135,6 +142,84 @@ class BMailtypeUpdate(object):
                     values.append(self.to_add[typ][key])
                 api.portal.set_registry_record(MAILTYPES[typ], values)
             self.storage['data']['p_mailtype'] = get_mailtypes(self.portal)
+
+
+class DOMSenderCreation(object):
+    """Create sender held_position if necessary.
+
+    Parameters:
+        * condition = O, condition expression
+    """
+    classProvides(ISectionBlueprint)
+    implements(ISection)
+
+    def __init__(self, transmogrifier, name, options, previous):
+        self.previous = previous
+        self.portal = transmogrifier.context
+        self.storage = IAnnotations(transmogrifier).get(ANNOTATION_KEY)
+        self.part = get_part(name)
+        if not is_in_part(self, self.part):
+            return
+        self.condition = Condition(options.get('condition') or 'python:True', transmogrifier, name, options)
+        self.puid_to_pers = self.storage['data']['p_userid_to_pers']
+        self.euid_to_pers = self.storage['data']['p_euid_to_pers']
+        self.p_hps = self.storage['data']['p_hps']
+        self.p_eid_orgs = self.storage['data']['p_eid_to_orgs']
+        self.intids = getUtility(IIntIds)
+
+    def __iter__(self):
+        for item in self.previous:
+            if is_in_part(self, self.part) and self.condition(item):
+                if item['_sender_id']:
+                    pass
+                else:
+                    if u'None' not in self.euid_to_pers:
+                        # we create a person
+                        path = correct_path(self.portal, os.path.join(self.storage['plone']['directory_path'],
+                                                                      'personnel-folder/reprise-donnees'))
+                        pdic = {'_type': 'person', '_path': path, u'internal_number': u'None', u'firstname': u'',
+                                u'lastname': u'Reprise données', u'_transitions': ('deactivate',),
+                                u'_etyp': u'person_sender', u'_post_actions': (u'store_internal_person_info', )}
+                        yield pdic
+                    puid = self.euid_to_pers[u'None']
+                    ouid = self.p_eid_orgs[item['_service']]
+                    if ouid not in self.p_hps[puid]['hps']:
+                        path = correct_path(self.portal, os.path.join(self.p_hps[puid]['path'], ouid))
+                        org = uuidToObject(ouid, unrestricted=True)
+                        hpdic = {'_type': 'organization', '_path': path, 'use_parent_address': True,
+                                 'position': RelationValue(self.intids.getId(org)), 'internal_number': u'',
+                                 u'_transitions': ('deactivate',), u'_etyp': u'hp_sender'}
+                        self.p_hps[puid]['hps'][ouid] = {'path': path, 'state': 'deactivated'}
+                        yield hpdic
+                continue
+            yield item
+
+
+class PostActions(object):
+    """Do post actions after creation"""
+    classProvides(ISectionBlueprint)
+    implements(ISection)
+
+    def __init__(self, transmogrifier, name, options, previous):
+        self.previous = previous
+        self.name = name
+        self.portal = transmogrifier.context
+        self.storage = IAnnotations(transmogrifier).get(ANNOTATION_KEY)
+
+    def __iter__(self):
+        for item in self.previous:
+            pa = item.get('_post_actions', [])
+            if u'store_internal_person_info' in pa:
+                eid = item[u'internal_number']
+                uid = self.portal.unrestrictedTraverse(item['_path']).UID()
+                self.storage['data']['p_euid_to_pers'][eid] = uid
+                self.storage['data']['p_hps'][uid] = {'path': item['_path'], 'eid': eid, 'hps': {},
+                                                      'state': 'deactivated'}
+            if u'store_internal_hp_info' in pa:
+
+                hps[puid]['hps'][ouid] = {'path': relative_path(portal, brain.getPath()),
+                                          'state': api.content.get_state(hp)}
+            yield item
 
 
 class StoreInData(object):
