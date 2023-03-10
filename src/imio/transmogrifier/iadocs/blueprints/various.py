b3e5584ffdd24e76b0bb524bcd3a9f40a73167e0
Stephan Geulette <s.geulette@imio.be>
2023-03-10 13:48:10 +0100

    Added enhanced imio inserter to add storage in expressions

--- src/imio/transmogrifier/iadocs/blueprints/various.py
+++ src/imio/transmogrifier/iadocs/blueprints/various.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function
+# from __future__ import print_function
+from collective.transmogrifier.utils import Expression
 from imio.transmogrifier.iadocs import o_logger
 from imio.transmogrifier.iadocs import ANNOTATION_KEY
 from collective.transmogrifier.interfaces import ISection
@@ -103,6 +104,25 @@ class EnhancedCondition(object):
                     yield item
             else:
                 yield item
+
+
+class EnhancedInserter(object):
+    classProvides(ISectionBlueprint)
+    implements(ISection)
+
+    def __init__(self, transmogrifier, name, options, previous):
+        self.key = Expression(options['key'], transmogrifier, name, options)
+        self.value = Expression(options['value'], transmogrifier, name, options)
+        self.condition = Condition(options.get('condition', 'python:True'), transmogrifier, name, options)
+        self.previous = previous
+        self.storage = IAnnotations(transmogrifier).get(ANNOTATION_KEY)
+
+    def __iter__(self):
+        for item in self.previous:
+            key = self.key(item)
+            if self.condition(item, key=key, storage=self.storage):
+                item[key] = self.value(item, key=key, storage=self.storage)
+            yield item
 
 
 class NeedOther(object):
