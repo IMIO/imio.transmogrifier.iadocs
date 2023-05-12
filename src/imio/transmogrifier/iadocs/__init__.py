70a74a3d0082cab74e7ec7d2c5d823548f39f83a
Stephan Geulette <s.geulette@imio.be>
2023-05-12 09:47:39 +0200

    Shorten logging default format

--- src/imio/transmogrifier/iadocs/__init__.py
+++ src/imio/transmogrifier/iadocs/__init__.py
@@ -9,13 +9,22 @@ import logging
 # types shortcuts
 T_S = {'dmsincomingmail': 'IM', 'dmsincoming_email': 'IE', 'dmsoutgoingmail': 'OM'}
 
+# logging.basicConfig(format='%(asctime)s %(name)s %(levelname).1s %(message)s',
+#                     datefmt='%y%m%d %H:%M:%S',
+#                     )
+root_logger = logging.getLogger()
+root_handler = root_logger.handlers[0]
+log_format = '%(asctime)s %(levelname).1s (%(name)s) %(message)s'
+root_handler.setFormatter(logging.Formatter(log_format))
+root_handler.formatter.datefmt = '%y%m%d %H%M%S'
+
 logger = logging.getLogger('dt')
 setup_logger(logger)   # needed to be displayed with instance run.
 
-e_logger = logging.getLogger('dt-input')
+e_logger = logging.getLogger('dti')
 setup_logger(e_logger)
 
-o_logger = logging.getLogger('dt-output')
+o_logger = logging.getLogger('dto')
 setup_logger(o_logger)
 
 ANNOTATION_KEY = 'imio.transmogrifier.iadocs'
