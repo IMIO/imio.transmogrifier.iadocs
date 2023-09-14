1841f4b7c0a14c326804f43f6cb383579b94adf8
Stephan Geulette <s.geulette@imio.be>
2023-09-14 14:03:48 +0200

    Sorted correctly followinf date

--- src/imio/transmogrifier/iadocs/scripts/mssqlcmd_to_fwf.py
+++ src/imio/transmogrifier/iadocs/scripts/mssqlcmd_to_fwf.py
@@ -18,7 +18,7 @@ tables = OrderedDict([
     ('XeAdresses', {}), ('eClassement', {}), ('eClassementDossiers', {}), ('eContacts', {}),
     ('eContactsAdresses', {}), ('eContactsTitres', {}), ('eContactsType', {}),
     ('eCourriers', {'c': "TypeEntrantSortant in ('E', 'S') and isnull(Supprime, '0') != '1'",
-                    'o': "DateEncodage, DateEntree"}),
+                    'o': "isnull(DateEncodage, dateentree), dateentree"}),
     ('eCourriersDestinataires', {}), ('eCourriersFichiers', {'o': "CourrierID, OrdreAffichage, DateUpload"}),
     ('eCourriersDossiers', {'o': "CourrierID, Principal desc"}),
     ('eCourriersLiens', {}), ('eCourriersServices', {'o': "CourrierID, Principal desc"}),
