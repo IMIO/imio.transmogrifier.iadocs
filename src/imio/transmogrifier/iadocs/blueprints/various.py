068f847a9742c033ed748e4aaeef6f3093d9433d
Stephan Geulette <s.geulette@imio.be>
2024-06-12 14:43:27 +0200

    Added EnhancedManipulator section (not finally used)

--- src/imio/transmogrifier/iadocs/blueprints/various.py
+++ src/imio/transmogrifier/iadocs/blueprints/various.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from collective.transmogrifier.interfaces import ISection
 from collective.transmogrifier.interfaces import ISectionBlueprint
+from collective.transmogrifier.utils import Matcher
 from imio.helpers.transmogrifier import Condition
 from imio.helpers.transmogrifier import Expression
 from imio.helpers.transmogrifier import filter_keys
@@ -19,6 +20,7 @@ from zope.annotation.interfaces import IAnnotations
 from zope.interface import classProvides
 from zope.interface import implements
 
+import copy
 import ipdb
 
 
@@ -197,6 +199,42 @@ class EnhancedInserter(object):
                         item[key] += u"{}{}".format(self.separator, value)
                 else:
                     item[key] = value
+            yield item
+
+
+class EnhancedManipulator(object):
+    """Manipulate item if condition is matched: copy, move or discard keys.
+
+    Parameters:
+        * keys = O, source keys. Can starts with re:
+        * dest = O, destination keys. Can be expression using keys pattern matching
+        * delete = O, keys to delete or move.
+        * condition = O, matching condition.
+    """
+
+    classProvides(ISectionBlueprint)
+    implements(ISection)
+
+    def __init__(self, transmogrifier, name, options, previous):
+        keys = options.get("keys") or ""
+        self.keys = Matcher(*keys.splitlines())
+        if keys:
+            self.dest = Expression(options["destination"], transmogrifier, name, options)
+        self.delete = Matcher(*options.get("delete", "").splitlines())
+        self.condition = Condition(options.get("condition", "python:True"), transmogrifier, name, options)
+        self.previous = previous
+
+    def __iter__(self):
+        for item in self.previous:
+            if self.condition(item):
+                course_store(self, item)
+                for key in item.keys():
+                    match = self.keys(key)[1]
+                    if match:
+                        dest = self.dest(item, key=key, match=match)
+                        item[dest] = copy.deepcopy(item[key])
+                    if self.delete(key)[1]:
+                        del item[key]
             yield item
 
 
